12956f605b67f9418e88725e97dfb18de12969e4
Jean-Louis Huynen <huynenjl@gmail.com>
2019-01-17 12:13:35 +0100

    new example scripts to get a root CA

--- server/gen_cert.sh
+++ server/gen_cert.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash 
-openssl genrsa -out server.key 2048
+openssl genrsa -out server.key 4096
 openssl req -sha256 -new -key server.key -out server.csr -subj '/CN=localhost'
 openssl x509 -req -sha256 -days 365 -in server.csr -signkey server.key -out server.crt
+openssl x509 -req -in server.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out server.crt -days 500 -sha256
 cat server.crt server.key > server.pem
+
