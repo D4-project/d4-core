f1e27cf4477810c5198a767ce4b15f3c6a01c4d7
Jean-Louis Huynen <huynenjl@gmail.com>
2019-01-18 08:59:15 +0100

    Adds scripts and instructions for Verifiable Self-Signed Certificate generation

--- server/gen_cert.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash 
-openssl genrsa -out server.key 4096
-openssl req -sha256 -new -key server.key -out server.csr -subj '/CN=localhost'
-openssl x509 -req -sha256 -days 365 -in server.csr -signkey server.key -out server.crt
-openssl x509 -req -in server.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out server.crt -days 500 -sha256
-cat server.crt server.key > server.pem
-
